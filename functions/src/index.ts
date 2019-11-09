467b5a8fa2253c2046866da98fdb8c311d05a503
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-11-09 23:11:17 +0900

    feat: impl mock api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,3 +6,18 @@ import * as functions from 'firebase-functions'
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!')
 })
+
+export const getVersions = functions.https.onRequest((_, response) => {
+  const versions = [
+    {
+      id: 'id1',
+      name: 'hoge'
+    },
+    {
+      id: 'id2',
+      name: 'fuga'
+    }
+  ]
+
+  response.send(versions)
+})
