9f89a08427a1cf087a848f12999fa4079d677411
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-11-10 00:44:14 +0900

    fix: remove unused code

--- app/src/main.elm
+++ app/src/main.elm
@@ -22,7 +22,7 @@ main =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { name = "", articles = [], jwt = "JWT_TOKEN", versions = [] }, Cmd.none )
+    ( { name = "", versions = [] }, Cmd.none )
 
 
 
@@ -30,20 +30,12 @@ init _ =
 
 
 type alias Model =
-    { name : String, articles : List Article, jwt : String, versions : List Version }
+    { name : String, versions : List Version }
 
 
 type Msg
-    = GetProfile
-    | GotProfile (Result Http.Error String)
-    | GetArticles
-    | GotArticles (Result Http.Error (List Article))
-    | ListVersionsRequest
+    = ListVersionsRequest
     | ListVersionsDone (Result Http.Error (List Version))
-
-
-type alias Article =
-    { id : Int, title : String, author : String }
 
 
 type alias Version =
@@ -57,32 +49,6 @@ type alias Version =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GetProfile ->
-            ( model
-            , getProfile
-            )
-
-        GotProfile result ->
-            case result of
-                Ok name ->
-                    ( { model | name = name }, Cmd.none )
-
-                Err _ ->
-                    ( model, Cmd.none )
-
-        GetArticles ->
-            ( model
-            , getArticles
-            )
-
-        GotArticles result ->
-            case result of
-                Ok articles ->
-                    ( { model | articles = articles }, Cmd.none )
-
-                Err _ ->
-                    ( model, Cmd.none )
-
         ListVersionsRequest ->
             ( model
             , listVersions
@@ -103,38 +69,12 @@ update msg model =
 
 view model =
     div []
-        [ button [ onClick GetProfile ] [ text "Get profile" ]
-        , p [] [ text ("name: " ++ model.name) ]
-        , button [ onClick GetArticles ] [ text "Get articles" ]
-        , div []
-            (articlesView
-                model.articles
-            )
-        , button [ onClick ListVersionsRequest ] [ text "Get versions" ]
+        [ button [ onClick ListVersionsRequest ] [ text "Get versions" ]
         , div []
             (versionsView
                 model.versions
             )
         ]
-
-
-articlesView : List Article -> List (Html msg)
-articlesView articles =
-    List.map
-        (\article ->
-            p []
-                [ text
-                    ("article: "
-                        ++ "id: "
-                        ++ String.fromInt article.id
-                        ++ " title: "
-                        ++ article.title
-                        ++ " author: "
-                        ++ article.author
-                    )
-                ]
-        )
-        articles
 
 
 versionsView : List Version -> List (Html msg)
@@ -156,58 +96,6 @@ versionsView versions =
 
 
 -- HTTP
-
-
-getProfile : Cmd Msg
-getProfile =
-    Http.request
-        { method = "GET"
-        , headers =
-            [ Http.header "Authorization" ("Bearer " ++ "jwt")
-            , Http.header "Accept" "application/json"
-            , Http.header "Content-Type" "application/json"
-            ]
-        , url = "http://localhost:3000/profile"
-        , expect = Http.expectJson GotProfile profileDecoder
-        , body = Http.emptyBody
-        , timeout = Nothing
-        , tracker = Nothing
-        }
-
-
-profileDecoder : Decoder String
-profileDecoder =
-    field "name" string
-
-
-getArticles : Cmd Msg
-getArticles =
-    Http.request
-        { method = "GET"
-        , headers =
-            [ Http.header "Authorization" ("Bearer " ++ "jwt")
-            , Http.header "Accept" "application/json"
-            , Http.header "Content-Type" "application/json"
-            ]
-        , url = "http://localhost:3000/articles"
-        , expect = Http.expectJson GotArticles articlesDecoder
-        , body = Http.emptyBody
-        , timeout = Nothing
-        , tracker = Nothing
-        }
-
-
-articleDecoder : Decoder Article
-articleDecoder =
-    Json.Decode.map3 Article
-        (Json.Decode.field "id" Json.Decode.int)
-        (Json.Decode.field "title" Json.Decode.string)
-        (Json.Decode.field "author" Json.Decode.string)
-
-
-articlesDecoder : Decoder (List Article)
-articlesDecoder =
-    Json.Decode.list articleDecoder
 
 
 listVersions : Cmd Msg
